7d7844f5ffcc3769828a2a1437b79c951eee726a
Hiruni Dewapriya <malshikahiruni99@gmail.com>
2024-11-19 00:28:38 +0530

    add product and backend connected

---
+++ app/addproduct/page.tsx
@@ -0,0 +1,74 @@
+"use client"
+import { useState } from "react";
+import api from '../../utils/api';
+
+export default function AddProductPage() {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [stock, setStock] = useState('');
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try{
+            const response = await api.post('/products', {
+                name,
+                description,
+                price,
+                stock
+            })
+            console.log('Product added: ', response.data);
+            alert('Product added successfully!')
+        } catch(error){
+            console.error('Error adding product: ', error);
+            alert('Failed to add product.')
+        }
+    };
+
+    return (
+        <div>
+            <h1>Add Product</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Product Name:</label>
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Product Description:</label>
+                    <input
+                        type="text"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Product Price:</label>
+                    <input
+                        type="number"
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Product Stock:</label>
+                    <input
+                        type="number"
+                        value={stock}
+                        onChange={(e) => setStock(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">Add Product</button>
+            </form>
+        </div>
+    )
+
+
+}
---
+++ app/products/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import api from '../../utils/api';
+
+
+interface Product {
+    id: number;
+    name: string;
+    description:string;
+    price: number;
+    stock: number;
+}
+
+export default function ProductsPage () {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try{
+                const response = await api.get<Product[]>('/products');
+                console.log(response.data)
+                setProducts(response.data);
+            } catch(error){
+                console.error('Error fetching products: , error');
+            }
+        };
+
+        fetchProducts();
+    }, [])
+
+    return (
+        <div>
+            <h1>Products</h1>
+            <ul>
+                {products.map((product) => (
+                    <li key={product.id}>
+                        {product.name} - ${product.price}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+
+}
